38123c930bb65a69e8c717c5d62624abbb9ef02a
Sarthak Joshi <joshi.sarthak@gmail.com>
2021-08-24 15:05:15 -0400

    Added axios for HTTP support

--- reactivities.client/src/App.tsx
+++ reactivities.client/src/App.tsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+
+// Todo : Read Axios and Promise MDN
 
 function App() {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      console.log(response);
+      setActivities(response.data);
+    });
+    // return () => {
+    //   cleanup;
+    // };
+  }, []);
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <ul>
+          {activities.map((activity: any) => (
+            <li key={activity.id}>
+              <label> Title : {activity.title}</label>
+              <label> Date : {activity.date}</label>
+              <label> Description : {activity.description}</label>
+              <label> Category : {activity.category}</label>
+              <label> City : {activity.city}</label>
+              <label> Venue : {activity.venue}</label>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// http://localhost:5000/api/activities
